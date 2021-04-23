0f28f775b10638f14e336ce9a16e6cebf7ffc0f1
timjden <60508756+timjden@users.noreply.github.com>
2021-04-23 10:46:26 +0200

    A few more updates

--- dashboard/benchling-dashboard.py
+++ dashboard/benchling-dashboard.py
@@ -93,29 +93,27 @@ def get_dashboard ():
     #Select yield per protein batch
     yieldQuery = """
 
-    WITH total_vol as (
-
-    SELECT h.file_registry_id$ as id, COUNT(h.file_registry_id$), SUM(c.homogenate_volume_l) as total_vol
+    WITH total_vol as
+    (SELECT h.file_registry_id$ as id,
+            COUNT(h.file_registry_id$),
+            SUM(c.homogenate_volume_l) as total_vol
     FROM clarificates c
     LEFT JOIN homogenates h ON h.id = c.homogenate
     GROUP BY h.file_registry_id$
-    ORDER BY h.file_registry_id$
-
-    )
-
+    ORDER BY h.file_registry_id$)
     SELECT CONCAT(pb.file_registry_id$, ' ', pb.name) AS "ID",
-
-    (CASE 
+       (CASE
             WHEN pb.name = 'S1-His' THEN ROUND((COALESCE((qc.volume_ul/1000)*blitz_mgml, 0)/ROUND(((c.homogenate_volume_l/total_vol.total_vol)*h.leaf_weight)::numeric, 2))::numeric, 2)
-        ELSE ROUND((COALESCE((qc.volume_ul/1000)*uv280_mgml, 0)/ROUND(((c.homogenate_volume_l/total_vol.total_vol)*h.leaf_weight)::numeric, 2))::numeric, 2) 
+            ELSE ROUND((COALESCE((qc.volume_ul/1000)*uv280_mgml, 0)/ROUND(((c.homogenate_volume_l/total_vol.total_vol)*h.leaf_weight)::numeric, 2))::numeric, 2)
         END) AS yield_mgkg
-
-    FROM homogenates h
-    LEFT JOIN clarificates c ON h.id = c.homogenate
+    FROM protein_batches pb
+    LEFT JOIN jsonb_array_elements_text(pb.clarificate_batch) AS cla_id ON TRUE
+    LEFT JOIN clarificates c ON cla_id = c.id
+    LEFT JOIN homogenates h ON c.homogenate = h.id
     LEFT JOIN total_vol ON h.file_registry_id$ = total_vol.id
-    LEFT JOIN protein_batches pb ON c.id = pb.clarificate_batch
     LEFT JOIN quality_control qc ON pb.id = qc.protein_batch
-    WHERE qc.date_time >= '""" + startRange + """' AND  qc.date_time <= '""" + endRange + """'
+    WHERE qc.date_time >= '""" + startRange + """'
+        AND qc.date_time <= '""" + endRange + """'
     ORDER BY pb.file_registry_id$
 
     """
